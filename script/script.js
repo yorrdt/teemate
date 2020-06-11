5045f6c24c8809c11994a52ef32048e408d6c168
Egor Moroz <44409352+yorrdt@users.noreply.github.com>
2020-06-11 22:58:06 +0300

    added illustration, page more adaptive

--- script/script.js
+++ script/script.js
@@ -1,11 +1,1 @@
-function setNavBarHeight() {
-    $('.navbar_container').css({
-        height: $(window).height() - $('#header_container').height() - 20 + 'px',
-    });
-	$('.content_container').css({
-        height: $(window).height() - $('#header_container').height() - 20 + 'px',
-    });
-}
-
-setNavBarHeight();
-$(window).resize(setNavBarHeight);
+console.log($(window).width());
