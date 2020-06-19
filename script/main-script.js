44aeb51d74bf7cf4ecb4e2e9586f68aa58cfacc6
Egor Moroz <44409352+yorrdt@users.noreply.github.com>
2020-06-19 16:36:16 +0300

    a little bit of changes

--- script/main-script.js
+++ script/main-script.js
@@ -1,2 +1,8 @@
-autosize(document.querySelectorAll('textarea'));
+autosize(document.querySelectorAll('.user-textarea'));
 
+function onButtonClick() {
+	let height = $('.user-textarea').height();
+	/* console.log(height); */
+	
+	$('.user-textarea').height(50);
+}
