c45cf63f22c36f670863dfb52b288d2a09b17a0d
yorrdt <this.mazahaka@gmail.com>
2020-07-24 22:57:37 +0300

    new design

--- teemate-vuejs/src/App.vue
+++ teemate-vuejs/src/App.vue
@@ -1,3 +1,9 @@
-<template src="./components/App.html"></template>
-<style src="./components/App.css"></style>
-<script src="./components/App.js"></script>
+<!-- 
+<template src="./components/index.html"></template>
+<style src="./components/style/index.css"></style>
+<script src="./components/script/index.js"></script> 
+-->
+
+<template src="./components/profile.html"></template>
+<style src="./components/style/profile.css"></style>
+<script src="./components/script/profile.js"></script>
