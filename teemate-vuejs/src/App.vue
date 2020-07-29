885cc59e88f01c255b4497616f05daad699cb7b5
yorrdt <this.mazahaka@gmail.com>
2020-07-29 23:49:07 +0300

    illustration for tm vuejs-cli, deleted webpack ver, teemate-native

--- teemate-vuejs/src/App.vue
+++
@@ -1,9 +0,0 @@
-<!-- 
-<template src="./components/index.html"></template>
-<style src="./components/style/index.css"></style>
-<script src="./components/script/index.js"></script> 
--->
-
-<template src="./components/profile.html"></template>
-<style src="./components/style/profile.css"></style>
-<script src="./components/script/profile.js"></script>
--- teemate-vuejs/src/components/script/profile.js
+++
@@ -1,8 +0,0 @@
-export default {
-	name: 'app',
-	data() {
-		return {
-			visible: false,
-		}
-	},
-}
