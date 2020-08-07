96454cafd0abf41b93c485144cd95f9102d13b82
yorrdt <this.mazahaka@gmail.com>
2020-08-07 17:28:34 +0300

    deleted teemate-vuejs-cli

--- teemate-vuejs-cli/src/views/index.vue
+++
@@ -1,3 +0,0 @@
-<template src="../components/index.html"></template>
-<style src="../components/style/index.css"></style>
-<script src="../components/script/index.js"></script> 
--- teemate-vuejs-cli/src/views/profile.vue
+++
@@ -1,3 +0,0 @@
-<template src="../components/profile.html"></template>
-<style src="../components/style/profile.css"></style>
-<script src="../components/script/profile.js"></script> 
